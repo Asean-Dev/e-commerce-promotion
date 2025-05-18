f6bfed5fb07f0d62bf2ef14ce0c3379f79a8277b
ASEAN <Sahasawat.4665@gmail.com>
2025-05-18 23:17:33 +0700

    [update] fix text

--- src/utils/calculate-promotion/_mock.ts
+++ src/utils/calculate-promotion/_mock.ts
@@ -68,7 +68,7 @@ export const renderCampaignDescription = (campaign: CampaignType) => {
       return `Discount of ${parameters.percentage}% on total price. Code: ${campaign.code}`;
 
     case "OnTopPoints":
-      return `you ${parameters.points} points for additional discount`;
+      return `you have ${parameters.points} points for and additional discount`;
     case "OnTopCategory":
       return `Get ${parameters.percentage}% off on ${parameters.category} category`;
 
